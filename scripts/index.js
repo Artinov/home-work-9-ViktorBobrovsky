f85a6f1eb8725e831b5453d41e5aed2f744b0322
vikbobsky <v_bobrovsky@mail.ru>
2017-03-15 19:45:14 +0200

    Disco created))

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,11 @@
 var ball = document.querySelector(".discoball");
-
+var arrayClasses = ["here", "goes", "our","awesome", "disco", "ball"];
+i = 0;
 function goDisco() {
-	//do your disco magic here !
-}
+	if(i<=5){
+		ball.setAttribute("class", "discoball " + arrayClasses[i]);
+		i++;
+	}else {i = 0};
+};
 
 setInterval(goDisco,300);
